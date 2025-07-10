609226979a466f1232c84b8cf23700e7e2152949
Abrar Shahriar <abrarshahriar9410@gmail.com>
2025-07-11 01:14:11 +0600

    Redirect path changed after borrowing

--- src/pages/BorrowBook.tsx
+++ src/pages/BorrowBook.tsx
@@ -37,7 +37,7 @@ const BorrowBook = () => {
             });
             setUploading(false);
             reset();
-            navigate(-1);
+            navigate("/borrow-summary");
         } else {
             toast.error(`${res.data.message}`, {
                 position: "top-center"
